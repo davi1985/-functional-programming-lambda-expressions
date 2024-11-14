59ca7ae2197df20899c4bf79db50c6ba9adfd1dc
Davi Silva <davisilvaphoto@gmail.com>
2024-11-13 21:24:36 -0300

    interface comparator

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -1,7 +1,23 @@
 package application;
 
+import entities.MyComparator;
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.0));
+        list.add(new Product("Notebook", 1200.0));
+        list.add(new Product("Tablet", 450.0));
+
+        list.sort(new MyComparator());
+
+        for (Product p : list) {
+            System.out.println(p);
+        }
     }
 }
---
+++ src/main/java/entities/Product.java
@@ -0,0 +1,54 @@
+package entities;
+
+import java.util.Objects;
+
+public class Product {
+
+    private String name;
+    private Double price;
+
+    public Product() {
+    }
+
+    public Product(String name, Double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+
+    @Override
+    public String toString() {
+        return name + ", " + String.format("%.2f", price);
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        if (this == object) return true;
+        if (object == null || getClass() != object.getClass()) return false;
+
+        Product product = (Product) object;
+        return Objects.equals(getName(), product.getName()) && Objects.equals(getPrice(), product.getPrice());
+    }
+
+    @Override
+    public int hashCode() {
+        int result = Objects.hashCode(getName());
+        result = 31 * result + Objects.hashCode(getPrice());
+        return result;
+    }
+}
