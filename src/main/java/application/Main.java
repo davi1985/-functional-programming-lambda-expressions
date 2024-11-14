bbba6a4a439126f20e66e74514bd640efaa9b783
Davi Silva <davisilvaphoto@gmail.com>
2024-11-13 21:32:22 -0300

    lambda expression into fn argument

--- src/main/java/application/Main.java
+++ src/main/java/application/Main.java
@@ -3,7 +3,6 @@ package application;
 import entities.Product;
 
 import java.util.ArrayList;
-import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -14,9 +13,7 @@ public class Main {
         list.add(new Product("Notebook", 1200.0));
         list.add(new Product("Tablet", 450.0));
 
-        Comparator<Product> comparator = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-
-        list.sort(comparator);
+        list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
 
         for (Product p : list) {
             System.out.println(p);
