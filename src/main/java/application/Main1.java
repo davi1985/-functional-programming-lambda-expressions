01c5177abb870b49fd896b188ce32f4271f28414
Davi Silva <davisilvaphoto@gmail.com>
2024-11-13 21:44:02 -0300

    example 3

--- src/main/java/application/Main1.java
+++ src/main/java/application/Main1.java
@@ -1,23 +1,25 @@
 package application;
 
-import java.util.Arrays;
+import entities.Product;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main1 {
 
-    public static int globalValue = 3;
-
-    public static void main(String[] args) {
-        int[] numbers = new int[]{3, 4, 5};
-        changeOddValues(numbers);
-
-        System.out.println(Arrays.toString(numbers));
+    public static int compareProducts(Product p1, Product p2) {
+        return p1.getPrice().compareTo(p2.getPrice());
     }
 
-    private static void changeOddValues(int[] numbers) {
-        for (int i = 0; i < numbers.length; i++) {
-            if (numbers[i] % 2 != 0) {
-                numbers[i] += globalValue;
-            }
-        }
+    public static void main(String[] args) {
+        List<Product> list = new ArrayList<>();
+
+        list.add(new Product("Tv", 900.0));
+        list.add(new Product("Notebook", 1200.0));
+        list.add(new Product("Tablet", 450.0));
+
+        list.sort(Main1::compareProducts);
+        list.forEach(System.out::println
+        );
     }
 }
